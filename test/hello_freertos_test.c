0cc8f4315eb42941ecfa7f3ca4adf2735fcb57e4
Adam Welsh <u1456456@utah.edu>
2026-09-22 15:35:46 -0600

    add blink logic tests

--- test/hello_freertos_test.c
+++ test/hello_freertos_test.c
@@ -2,17 +2,25 @@
 #include <pico/stdlib.h>
 #include <stdint.h>
 #include <unity.h>
+#include <stdbool.h>
 #include "unity_config.h"
 #include "hello_fr_helpers.h"
+
+#include "pico/stdlib.h"
+#include "pico/multicore.h"
+#include "pico/cyw43_arch.h"
 
 
 void test_char(char c);
 
 void setUp(void) {}
 
-void tearDown(void) {}
+void tearDown(void) {
 
-void test_variable_assignment()
+
+}
+
+void test_variable_assignment(void)
 {
     int x = 1;
     TEST_ASSERT_TRUE_MESSAGE(x == 1,"Variable assignment failed.");
@@ -29,22 +37,54 @@ void test_multiplication(void)
 void test_char(char c){
     char actual = char_case_reverse(c);
     char expected = (c <= 'z' && c >= 'a') ? c-32 : c+32;
-    TEST_ASSERT_EQUAL_CHAR_MESSAGE(actual, expected, "ERROR: actual result");
+    // printf("Expected: %c\n", expected);
+    // printf("Actual: %c\n", actual);
+    TEST_ASSERT_EQUAL_CHAR_MESSAGE(expected, actual, "ERROR: actual result");
+}
+
+void test_non_letter_char(char c) {
+    char actual = char_case_reverse(c);
+    char expected = c;
+    TEST_ASSERT_EQUAL_CHAR_MESSAGE(expected, actual, "ERROR: expected result differes from actual result for normal non letter char");
+
 }
 
 void test_char_case_logic()
 {
-    for (int i = 0; i <= 26; i++) {
+    for (int i = 0; i < 26; i++) {
         char t_char = 65 + i;
         test_char(t_char);
     }
 
-    for (int i = 0; i <= 26; i++) {
+    for (int i = 0; i < 26; i++) {
         char t_char = 97 + i; // 97 (A)
         test_char(t_char);
     }
+
+    for (int i = 33; i < 65; i++) {
+        test_non_letter_char((char) i);
+    }
 }
 
+void test_light_switch_should_toggle()
+{
+    bool on;
+    int test_count = 11;
+    bool pre_toggle = on; 
+
+    on = toggle_led(test_count);  // with test count == 11, on should not toggle
+    TEST_ASSERT_EQUAL_CHAR_MESSAGE(pre_toggle, on, "ERROR: expected led on status diffeered from actual. LED Toggled when it shouldnt have");
+}
+
+void test_light_switch_dont_toggle(void){
+
+    int test_count = 12;
+    bool on;
+    bool pre_toggle = on;
+
+    on = toggle_led(test_count);
+    TEST_ASSERT_NOT_EQUAL_CHAR_MESSAGE(pre_toggle, on, "ERROR: expected led on status diffeered from actual. LED did not toggle when it should have");
+}
 int main (void)
 {
     stdio_init_all();
@@ -55,6 +95,8 @@ int main (void)
         RUN_TEST(test_variable_assignment);
         RUN_TEST(test_multiplication);
         RUN_TEST(test_char_case_logic);
+        RUN_TEST(test_light_switch_should_toggle);
+        RUN_TEST(test_light_switch_dont_toggle);
         sleep_ms(5000);
         UNITY_END();
     }
