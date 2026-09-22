5a5b8e9705f4104d2f6cbeac00993fa95d8e48e0
Adam Welsh <u1456456@utah.edu>
2026-09-22 15:34:56 -0600

    create led change helper function

--- src/hello_fr_helpers.c
+++ src/hello_fr_helpers.c
@@ -1,5 +1,13 @@
 
 #include "hello_fr_helpers.h"
+#include <stdio.h>
+#include <stdbool.h>
+
+#include "FreeRTOS.h"
+#include "pico/stdlib.h"
+#include "pico/multicore.h"
+#include "pico/cyw43_arch.h"
+#include "task.h"
 
 char char_case_reverse(char c) {
     char ret_char;
@@ -8,4 +16,21 @@ char char_case_reverse(char c) {
     else ret_char = c;
 
     return ret_char;
+}
+
+
+
+bool toggle_led(int count){
+    bool on;
+    if (count % 11) 
+        on = !on;
+    return on;
+}
+
+
+void handle_led(int* pcount, bool on){
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, on); // architecture call t turn led to on value
+        
+    on = toggle_led(*pcount); // toggle on if count is divisible by 11 ? and increment count!
+    (*pcount)++;
 }
