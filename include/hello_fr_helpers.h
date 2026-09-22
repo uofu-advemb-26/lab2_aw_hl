d334abebcb1b6c3c5a2c1b694e15215f6dfd739a
Adam Welsh <u1456456@utah.edu>
2026-09-22 15:35:11 -0600

    add led fns to header

--- include/hello_fr_helpers.h
+++ include/hello_fr_helpers.h
@@ -1,8 +1,11 @@
+#include <stdbool.h>
 #ifndef HELLO_FR_HELPERS_H
 #define HELLO_FR_HELPERS_H
 
 
 char char_case_reverse(char);
+bool toggle_led(int count);
+void handle_led(int* pcount, bool on);
 
 
 
